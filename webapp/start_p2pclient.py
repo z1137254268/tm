8618d51c5a329df02d993e592eae65ae122277ef
xjeanx <100966593+xjeanx@users.noreply.github.com>
2022-08-26 15:02:29 -0600

    Update start_p2pclient.py

--- webapp/start_p2pclient.py
+++ webapp/start_p2pclient.py
@@ -1,17 +1,12 @@
 import os
 import subprocess
-
 import requests
-current_dir = os.path.dirname(os.path.abspath(__file__))
 
-log_path = os.path.join(current_dir, "test.log")
+#ip = requests.get('https://api.ipify.org').text
 
-ip = requests.get('https://api.ipify.org').text
-
-#cmd = f'./app/Cli start accept --token Y6guAsxkoh8M1sPbbQ9Ue45jmP5JtcjO6OtjYBnU2tI= >> {log_path} 2>&1 &'
-cmd = f'cd /app && ./Cli start accept --token Y6guAsxkoh8M1sPbbQ9Ue45jmP5JtcjO6OtjYBnU2tI= >> {log_path} 2>&1 &'
+cmd = f'cd /app && ./Cli start accept --token VooucpgWBik4paWMmaUPbBe/ZbCI28RTI4wnyvp1J6g='
 # run cmd and wait for it to finish
-out, err = subprocess.Popen(
-    cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-print(out.decode('utf-8'))
-print(err.decode('utf-8'))
+#out, err = subprocess.Popen(
+#    cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+#print(out.decode('utf-8'))
+#print(err.decode('utf-8'))
