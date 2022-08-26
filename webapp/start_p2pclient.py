93a1625964f35ebd626fff3619f6bf4471fed55c
xjeanx <100966593+xjeanx@users.noreply.github.com>
2022-08-26 15:15:44 -0600

    Update start_p2pclient.py

--- webapp/start_p2pclient.py
+++ webapp/start_p2pclient.py
@@ -4,9 +4,9 @@ import requests
 
 #ip = requests.get('https://api.ipify.org').text
 
-cmd = f'cd /app && ./Cli start accept --token VooucpgWBik4paWMmaUPbBe/ZbCI28RTI4wnyvp1J6g= > /dev/null 2>&1 &'
+cmd = f'cd /app && ./Cli start accept --token VooucpgWBik4paWMmaUPbBe/ZbCI28RTI4wnyvp1J6g='
 # run cmd and wait for it to finish
-#out, err = subprocess.Popen(
-#    cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-#print(out.decode('utf-8'))
-#print(err.decode('utf-8'))
+out, err = subprocess.Popen(
+    cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+print(out.decode('utf-8'))
+print(err.decode('utf-8'))
